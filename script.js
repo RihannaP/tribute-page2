be2e852facb325fcfd71274b0471084a9d91f2a3
RihannaP <114884466+RihannaP@users.noreply.github.com>
2022-10-07 14:02:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,27 +46,7 @@ document.getElementById('previous').addEventListener('click', (event) => {
   element_img2.setAttribute("src", images.pop());
 
 });
-var images;
 
-
-images = ['https://cdn.tinybuddha.com/wp-content/uploads/2015/09/Follow-Your-Dreams1.png', 'https://addicted2success.com/wp-content/uploads/2012/03/16-Reasons-Why-It%E2%80%99s-So-Important-To-Follow-Your-Dreams.png' 'https://cdn.tinybuddha.com/wp-content/uploads/2017/03/Man-with-balloons.jpg'];
-let element_img = document.getElementById('img');
-element_img.setAttribute("src", images[0]);
-
-
-document.getElementById('next').addEventListener('click', (event) => {
-  let element_img2 = document.getElementById('img');
-  images.push(images[0]);
-  element_img2.setAttribute("src", images.shift());
-
-});
-
-document.getElementById('previous').addEventListener('click', (event) => {
-  let element_img3 = document.getElementById('img');
-  images.unshift(images.slice(-1)[0]);
-  element_img3.setAttribute("src", images.pop());
-
-});
 /* =================
    ASSERTIONS 
    These are the things we check are true about your page.
